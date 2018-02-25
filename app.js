6d863ddee3125aa340737e35df36276e72b1dc50
Angga Pramudya <pramudya.angga58@gmail.com>
2018-02-25 19:30:06 +0700

    Adding db config variable to app.js

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+var db = require('./model/db');
+var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
